b74b97cf698dc07dc012e71bc53425469571d41f
houtankeara17 <houtankeara17@gmail.com>
2025-08-26 17:37:31 +0700

    Add file main.js

---
+++ src/main/main.js
@@ -0,0 +1,7 @@
+console.log('Hello, Quiz App!');
+import { createApp } from 'vue';
+import App from './App.vue';   
+import router from './router';
+import store from './store';
+import './assets/main.css';
+import 'animate.css';
