3c41803ae06d18ad8288b8bd839919f55d34caf4
houtankeara17 <houtankeara17@gmail.com>
2025-08-26 17:54:18 +0700

    Add new file and folder routes/routes.js

---
+++ src/routes/route.js
@@ -0,0 +1,9 @@
+console.log("Hello, Quiz App!");
+import { createRouter, createWebHistory } from 'vue-router';
+import Home from '../views/Home.vue';
+import Quiz from '../views/Quiz.vue';
+import Result from '../views/Result.vue';
+import NotFound from '../views/NotFound.vue';
+import About from '../views/About.vue';
+import Contact from '../views/Contact.vue';
+import Profile from '../views/Profile.vue';
